adc193f546db397f865ce1a1dd89976fb4dea1f2
Tejas Birje <tejasbirje30@gmail.com>
2025-04-17 16:46:52 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,13 +26,31 @@ app.use("/api/messages", messageRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
+
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-      });
-      
-  }
+
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+
+    app.get("/login", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+
+    app.get("/signup", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+
+    app.get("/profile", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+
+    app.get("/settings", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
